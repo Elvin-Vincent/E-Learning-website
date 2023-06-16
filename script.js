98451ad0e97a4e8bb4c72d38085400a5683b59e8
Elvin Vincent A <84258439+Elvin-Vincent@users.noreply.github.com>
2023-06-16 19:16:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
+document.getElementById("card").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent form submission
 
   // Fetch user input values
-  var email = document.getElementById("email").value;
+  var email = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
   // Perform login validation
   if (email === "example1@gmail.com" && password === "password") {
     alert("Login successful!");
-    window.location.href = "submit.html"; // Redirect to index.html
+    window.location.href = "submit.html"; // Redirect to submit.html
   } else if (email === "example2@gmail.com" && password === "password") {
-    alert("login successful!");
-    window.location.href = "submit.html"; // Redirect to admin.html
+    alert("Login successful!");
+    window.location.href = "admin.html"; // Redirect to admin.html
   } else {
     alert("Invalid email or password. Please try again.");
   }
 });
-
   
