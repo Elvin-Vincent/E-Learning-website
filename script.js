009cd71c99d19a4125d92538b177101076bb90de
Elvin Vincent A <84258439+Elvin-Vincent@users.noreply.github.com>
2023-06-18 20:59:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ document.getElementById("card").addEventListener("submit", function(event) {
   // Perform login validation
   if (email === "example1@gmail.com" && password === "password") {
     alert("Login successful!");
-    window.location.href = "submit.html"; // Redirect to submit.html
+    window.location.href = "https://script.google.com/macros/s/AKfycbzxTIWmmbbm5vuEqMvnuW1MyPo3s6zHiQxbD0dCFpDSfjO6Y47nGZKD4nH4TJftma5b/exec"; // Redirect to the provided link
   } else if (email === "example2@gmail.com" && password === "password") {
     alert("Login successful!");
-    window.location.href = "admin.html"; // Redirect to admin.html
+    window.location.href = "https://script.google.com/macros/s/AKfycbzxTIWmmbbm5vuEqMvnuW1MyPo3s6zHiQxbD0dCFpDSfjO6Y47nGZKD4nH4TJftma5b/exec"; // Redirect to the provided link
   } else {
     alert("Invalid email or password. Please try again.");
   }
 });
-  
+
