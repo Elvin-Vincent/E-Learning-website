f7a430780d425cd68d563eb6d0a52273811f9367
Elvin Vincent A <84258439+Elvin-Vincent@users.noreply.github.com>
2023-06-22 15:50:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ document.getElementById("card").addEventListener("submit", function (event) {
   if (email === "example1@gmail.com" && password === "password") {
     alert("Login successful!");
     window.location.href =
-      "https://script.google.com/macros/s/AKfycbznk0XS2S1qtyiHvfBeE3z2UOhzrRjavXzxxeShJVIjinQHLY8TyLVF5m-s0_Tp6eg/exec"; // Redirect to the provided link
+      "https://script.google.com/macros/s/AKfycbyzX3ALbTi1BnP3cUNJaKVr4T3YqNNWPLBWDiZRkGDRy62RIJmynBq4T8csr5lc5MjJ/exec"; // Redirect to the provided link
   } else if (email === "example2@gmail.com" && password === "password") {
     alert("Login successful!");
     window.location.href =
-      "https://script.google.com/macros/s/AKfycbznk0XS2S1qtyiHvfBeE3z2UOhzrRjavXzxxeShJVIjinQHLY8TyLVF5m-s0_Tp6eg/exec"; // Redirect to the provided link
+      "https://script.google.com/macros/s/AKfycbyzX3ALbTi1BnP3cUNJaKVr4T3YqNNWPLBWDiZRkGDRy62RIJmynBq4T8csr5lc5MjJ/exec"; // Redirect to the provided link
   } else {
     alert("Invalid email or password. Please try again.");
   }
